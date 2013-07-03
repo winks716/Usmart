fd14909bb75d30681bbc0f3b00eb7eaecf1e4f55
Vincent.Shi <uatmestudio@163.com>
2013-07-03 11:32:56 +0800

    20130703 office combine2

--- js/codiqa.ext.js
+++ js/codiqa.ext.js
@@ -120,3 +120,35 @@ window.CodiqaControls.define('googlemaps', CodiqaControls.GoogleMap);
   window.CodiqaControls.init();
   
 })(jQuery);
+
+
+//customized action
+(function($){
+	
+	    //onSuccess Callback
+	    //This method accepts a Position object, which contains the
+	    //current GPS coordinates
+	    //
+	    var onSuccess = function(position) {
+	       $('#geoResult').html('Latitude: '          + position.coords.latitude          + '\n' +
+	             'Longitude: '         + position.coords.longitude         + '\n' +
+	             'Altitude: '          + position.coords.altitude          + '\n' +
+	             'Accuracy: '          + position.coords.accuracy          + '\n' +
+	             'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+	             'Heading: '           + position.coords.heading           + '\n' +
+	             'Speed: '             + position.coords.speed             + '\n' +
+	             'Timestamp: '         + position.timestamp                + '\n');
+	    };
+	    
+	    //onError Callback receives a PositionError object
+	    //
+	    function onError(error) {
+	        $('#geoResult').html('code: '    + error.code    + '\n' +
+	             'message: ' + error.message + '\n');
+	    }
+	    
+	    $('#geoPage').bind('click',function(){
+	    	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	    });
+
+})(jQuery);
